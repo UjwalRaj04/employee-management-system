5aef5ae0aeedfda0e8bcb7ccecd1e4ff9f4db0cd
UjwalRaj04 <ujwalrajkv11@gmail.com>
2026-08-30 22:48:52 +0100

    testing db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,6 +5,13 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected successfully");
+     console.log("DATABASE:", mongoose.connection.name);
+
+        console.log(
+            "COLLECTIONS:",
+            (await mongoose.connection.db.listCollections().toArray())
+                .map(c => c.name)
+        );
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
