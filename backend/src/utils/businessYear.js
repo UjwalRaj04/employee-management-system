15f2476bcc5604fb05229ae0a3bb26759beddfab
UjwalRaj04 <ujwalrajkv11@gmail.com>
2026-09-02 22:59:31 +0100

    created BusinessYear

---
+++ backend/src/utils/businessYear.js
@@ -0,0 +1,17 @@
+const { get } = require("mongoose");
+
+const getBusinessYear = (date = new Date()) => {
+    const currentDate = new Date(Date);
+
+    const year = currentDate.getFullYear();
+
+    const month = currentDate.getMonth();
+
+    const startYear = month < 3 ? year - 1 : year;
+
+    const endYear=startYear+1;
+
+    return `${startYear}/${String(endYear).slice(-2)}`
+};
+
+module.exports=getBusinessYear;
