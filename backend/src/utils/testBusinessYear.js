2fbce49a636e01839f7385ede2e1b127d841e86d
UjwalRaj04 <ujwalrajkv11@gmail.com>
2026-09-04 20:03:57 +0100

    updated test

---
+++ backend/src/utils/testBusinessYear.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const BusinessYear = require("../models/businessYear");
+require("dotenv").config();
+const createBusinessYear = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB Connected");
+
+        const businessYear = await BusinessYear.create({
+            year: "2026/27",
+            payIncreaseAmount: 0.20,
+            payIncreaseApplied: false,
+        });
+
+        console.log("Business Year Created");
+        console.log(businessYear);
+
+        await mongoose.connection.close();
+    }
+    catch (error) {
+        console.error("Error:", error);
+
+        await mongoose.connection.close();
+    }
+}
+
+createBusinessYear()
